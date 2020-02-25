c0e4a3034feb77fc492a9f4fffad2eb8003ae5f5
Richard Joseph <richjoseph1129@gmail.com>
2020-02-24 21:09:57 -0500

    Initial

--- ftserver.cpp
+++ ftserver.cpp
@@ -2,3 +2,13 @@
 // Created by Richard Joseph on 2/24/20.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
